339dccee2f0ccc8987c39ec5e1ceb047d3af8963
FARBOS Arnaud <farbos.arnaud@gmail.com>
2014-07-25 11:52:51 +0200

    Add chronos

--- src/main.cc
+++ src/main.cc
@@ -1,3 +1,17 @@
+#include <chrono>
+#include <iostream>
+
 int main()
 {
+  const std::chrono::time_point<std::chrono::system_clock> start
+    = std::chrono::system_clock::now();
+
+  // code
+
+  const std::chrono::time_point<std::chrono::system_clock> end
+    = std::chrono::system_clock::now();
+  std::chrono::duration<double> elapsed_seconds = end - start;
+  std::cout << "Execution time : " << elapsed_seconds.count()
+	    << " sec." << std::endl;
+
 }
