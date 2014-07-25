02a901d5d6d6a6beff91c84e09041aaf48d67e5a
FARBOS Arnaud <farbos.arnaud@gmail.com>
2014-07-25 11:38:17 +0200

    add generator program

---
+++ example/generator.cc
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <stdlib.h>
+#include <opencv2/photo/photo.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/core/core.hpp>
+
+int main(int argc, char** argv)
+{
+  if( argc != 3)
+  {
+    std::cout <<" Usage: " << argv[0]  <<" nb_color ouput_img" << std::endl;
+    return -1;
+  }
+
+  srand (time(NULL));
+  unsigned int size = atoi(argv[1]);
+  std::vector<cv::Vec3b> colors;
+
+  for(unsigned int i = 0; i < size; ++i)
+  {
+    colors.emplace_back(rand() % 256, rand() % 256, rand() % 256);
+  }
+
+  unsigned int width = 600;
+  unsigned int height = 800;
+
+  cv::Mat img(width, height, cv::DataType<cv::Vec3b>::type);
+
+  for (unsigned int i = 0; i < img.rows; ++i)
+    for (unsigned int j = 0; j < img.cols; ++j)
+    {
+      img.at<cv::Vec3b>(i, j) = colors[rand() % colors.size()];
+    }
+
+
+  cv::imwrite(argv[2], img);
+}
